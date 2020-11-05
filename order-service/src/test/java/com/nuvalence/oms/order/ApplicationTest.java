a3f9f898474449671532061c54ff4dec1eaa88e3
bedwards8000 <bedwards@nuvalence.io>
2020-11-05 13:45:53 -0500

    Add skeleton for order service

---
+++ order-service/src/test/java/com/nuvalence/oms/order/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.nuvalence.oms.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
